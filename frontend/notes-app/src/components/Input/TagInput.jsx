5f16d9b1037348d89d5545f7733bc9abeb6363a8
Faz Khalid <77042090+Fazzy05@users.noreply.github.com>
2024-10-11 15:22:03 +0500

    Getting Started with Backend

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -23,7 +23,7 @@ const TagInput = ({tags, setTags}) => {
         }
     };
 
-    const handleRemoveTag = () => {
+    const handleRemoveTag = (tagToRemove) => {
         setTags(tags.filter((tag) => tag !== tagToRemove));
     };
 
@@ -32,7 +32,7 @@ const TagInput = ({tags, setTags}) => {
 
         {tags?.length > 0 && (<div className='flex items-center gap-2 flex-wrap mt-2'>
             {tags.map((tag, index) => (
-                <span key={index} className=''>
+                <span key={index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'>
                     #{tag}
                     <button onClick={() => {
                         handleRemoveTag(tag);
@@ -47,6 +47,7 @@ const TagInput = ({tags, setTags}) => {
         <div className='flex items-center gap-4 mt-3'>
             <input 
             type="text" 
+            value={inputValue}
             className='text-sm bg-transparent border px-3 py-2 rounded outline-none' 
             placeholder='Add tags' 
             onChange={handleInputValue}
