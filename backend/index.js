fbecfe4aa3408499d162c8d8c12cfeda160f43ff
Faz Khalid <77042090+Fazzy05@users.noreply.github.com>
2024-10-15 14:17:39 +0500

    Pinned Note Api

--- backend/index.js
+++ backend/index.js
@@ -234,5 +234,33 @@ app.delete('/delete-note/:noteId', authenticateToken, async (req, res) => {
     }
 });
 
+//Update isPinned Value
+app.put('/update-note-pinned/:noteId', authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+    
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note){
+            return res
+            .status(400)
+            .json({error : true, message : "Note not Found!"});
+        }
+
+        if (isPinned) note.isPinned = isPinned || false;
+
+        await note.save();
+
+        return res
+        .json({error : true, note, message : "Note Updated Successfully"});
+    } catch (error) {
+        return res
+        .status(400)
+        .json({error : true, message : "Internal Server Error!"});
+    }
+});
+
 app.listen(8000);
 module.exports = app;
