a600f4f98ac19f1c2fd007439bf333fad6359514
Faz Khalid <77042090+Fazzy05@users.noreply.github.com>
2024-10-15 11:47:52 +0500

    Edit Note Api

--- backend/index.js
+++ backend/index.js
@@ -154,5 +154,41 @@ app.post('/add-note', authenticateToken, async (req, res) => {
     }
 });
 
+//Edit Note
+app.put('/edit-note/:noteId', authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+    if(!title && !content && !tags){
+        return res
+        .status(400)
+        .json({error : true, message : "No Changes were Found!"});
+    }
+
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note){
+            return res
+            .status(400)
+            .json({error : true, message : "Note not Found!"});
+        }
+
+        if (title) note.title = title;
+        if (content) note.content = content;
+        if (tags) note.tags = tags;
+        if (isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res
+        .json({error : true, note, message : "Note Updated Successfully"});
+    } catch (error) {
+        return res
+        .status(400)
+        .json({error : true, message : "Internal Server Error!"});
+    }
+});
+
 app.listen(8000);
 module.exports = app;
